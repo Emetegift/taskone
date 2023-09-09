3355b4de6ea27c01f22b6cbe90e9f0ec43f40821
Emete Gift <emeteokeoghene@gmail.com>
2023-09-09 10:26:45 +0100

    Solution to task one

---
+++ task/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('task1/', views.endpoint_view, name='endpoint_view'),
+    #  path('', views.endpoint_view, name='endpoint_view')
+]
---
+++ task/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
+import datetime
+
+@require_GET
+def endpoint_view(request):
+    slack_name = request.GET.get('slack_name', 'Gift Emete')
+    track = request.GET.get('track', 'backend')
+
+    # Get current day of the week
+    current_day = datetime.datetime.now().strftime("%A")
+
+    # Get current UTC time
+    current_utc_time = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    # Replace 'username', 'repo', and 'file_name.ext' with your actual GitHub details
+    github_file_url = "https://github.com/Emetegift/repo/blob/main/hgnx-task1.ext"
+    github_repo_url = "git remote add origin https://github.com/Emetegift/hgnx-task1.git"
+
+    response_data = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": current_utc_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
+    }
+
+    return JsonResponse(response_data)
